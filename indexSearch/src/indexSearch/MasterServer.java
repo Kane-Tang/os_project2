257444cda6156094e9ebb9a27d3f10f1d473a2cc
Kaiyuan <Kaiyuan@192.168.0.14>
2018-11-30 00:09:34 -0500

    thread

--- indexSearch/src/indexSearch/MasterServer.java
+++ indexSearch/src/indexSearch/MasterServer.java
@@ -1,5 +1,8 @@
 package indexSearch;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MasterServer {
 	public static void main(String[] args) {
 		if(args.length != 1) {
@@ -7,5 +10,11 @@ public class MasterServer {
 			System.exit(1);
 		}
 		int portNum = Integer.parseInt(args[0]);
+		ExecutorService threadPool = Executors.newFixedThreadPool(10);
+		threadPool.submit(new Server(8888,1));
+		threadPool.submit(new Server(8889,2));
+		threadPool.submit(new Server(8890,3));
+		threadPool.submit(new Server(8891,4));
+		threadPool.submit(new Server(8892,5));
 	}
 }
--- indexSearch/src/indexSearch/Server.java
+++ indexSearch/src/indexSearch/Server.java
@@ -7,18 +7,20 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
-
-	public static void main(String[] args) throws IOException {
-		if (args.length != 1) {
-			System.err.println("Usage: java EchoServer <port number>");
-			System.exit(1);
-		}
-
-		int portNum = Integer.parseInt(args[0]);
+public class Server implements Runnable{
+	private final int portNum;
+	private final int id;
+	
+	Server(int pn,int id){
+		this.portNum = pn;
+		this.id = id;
+	}
+	
+	@Override
+	public void run() {
 		try 
 		{
-			ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+			ServerSocket serverSocket = new ServerSocket(this.portNum);
 			Socket clientSocket = serverSocket.accept();
 			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -30,8 +32,34 @@ public class Server {
 			}
 		} catch (IOException e) {
 			System.out.println(
-					"Exception caught when trying to listen on port " + portNum + " or listening for a connection");
+					"Exception caught when trying to listen on port " + this.portNum + " or listening for a connection");
 			System.out.println(e.getMessage());
 		}
 	}
+
+//	public static void main(String[] args) throws IOException {
+//		if (args.length != 1) {
+//			System.err.println("Usage: java EchoServer <port number>");
+//			System.exit(1);
+//		}
+//
+//		int portNum = Integer.parseInt(args[0]);
+//		try 
+//		{
+//			ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+//			Socket clientSocket = serverSocket.accept();
+//			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+//			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//			String inputLine;
+//			while ((inputLine = in.readLine()) != null) {
+//				Search s = new Search();
+//				out.println(s.search(inputLine));
+//				out.println(inputLine);
+//			}
+//		} catch (IOException e) {
+//			System.out.println(
+//					"Exception caught when trying to listen on port " + this.portNum + " or listening for a connection");
+//			System.out.println(e.getMessage());
+//		}
+//	}
 }
